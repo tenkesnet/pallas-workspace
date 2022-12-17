0abc6e46e9214e3292227416ace706eff4439eef
tenkesnet <rbardo@gmail.com>
2022-12-17 13:22:51 +0100

    2022.12.17 - Fix recirsion

--- alapozo/src/main/java/org/pallas/alapozo/model/Alkalmazott.java
+++ alapozo/src/main/java/org/pallas/alapozo/model/Alkalmazott.java
@@ -2,8 +2,12 @@ package org.pallas.alapozo.model;
 
 import java.util.Date;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -27,7 +31,11 @@ public class Alkalmazott {
 	@Column(nullable = true)
 	public Integer premium;
 	public Date belepes;
-	@ManyToOne	
+	@ManyToOne(fetch=FetchType.EAGER)	
 	@JoinColumn(name="RESZLEG_ID")
-	public Reszleg reszleg;
+	private Reszleg reszleg;
+	
+	public String getReszelg() {
+		return reszleg.reszlegNev;
+	}
 }
--- alapozo/src/main/java/org/pallas/alapozo/model/Reszleg.java
+++ alapozo/src/main/java/org/pallas/alapozo/model/Reszleg.java
@@ -3,8 +3,13 @@ package org.pallas.alapozo.model;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -26,7 +31,26 @@ public class Reszleg {
 	@Column(length = 15, nullable = false)
 	public String reszlegCim;
 	
-	@OneToMany(mappedBy = "reszleg")
-	public List<Alkalmazott> alkalmazottak = new ArrayList<>();
+	@OneToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER,mappedBy = "reszleg")
+	private List<Alkalmazott> alkalmazottak = new ArrayList<>();
+	
+	public List<AlkalmazottView> getAlkalmazottak(){
+		ArrayList<AlkalmazottView> persons = new ArrayList<AlkalmazottView>();
+		for( Alkalmazott a : alkalmazottak) {
+			
+			persons.add(new AlkalmazottView(a.id,a.alkNev));
+		}
+		return persons;
+	}
+	
+	class AlkalmazottView {
+		public int id;
+		public String alkNev;
+		
+		public AlkalmazottView(int id, String nev) {
+			this.id = id;
+			this.alkNev = nev;
+		}
+	}
 	
 }
