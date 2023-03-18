04a6730a45f470aed52e6f47a79b02e0d94f2bc9
tenkesnet <rbardo@gmail.com>
2023-03-18 07:05:21 +0100

    Kristóf verzió

--- alapozo/src/main/java/org/pallas/alapozo/ImportKerdesek.java
+++ alapozo/src/main/java/org/pallas/alapozo/ImportKerdesek.java
@@ -11,6 +11,7 @@ import java.util.regex.Pattern;
 public class ImportKerdesek {
 
     final String importedFile = "/home/bardo/repo/pallas/backend/example2/alapozo/src/main/resources/java_kerdesek.txt";
+    public List<VizsgaKerdes> vizsgakerdesek;
 
     public void readFile() {
         List<String> text = null;
@@ -23,39 +24,52 @@ public class ImportKerdesek {
         Pattern p_kerdesszam = Pattern.compile("^([0-9]*).");
         Pattern p_megoldas = Pattern.compile("^(Megoldás:\\s*)([a-m]*)");
         Pattern p_valaszSzam = Pattern.compile("^([a-m])\\)");
-        boolean megoldasflag=true;
-        boolean valaszflag=false;
-        String valasz="";
-        String valaszSzam="";
-        int kerdesSzam=0;
-        List<VizsgaKerdes> vizsgakerdesek = new ArrayList<>();
-        List<VizsgaValasz> vizsgavalaszok = null;
+        boolean megoldasflag = true;
+        boolean valaszflag = false;
+        String valasz = "";
+        String kerdes = "";
+        String valaszSzam = "";
+        String megoldas = "";
+        int kerdesSzam = 0;
+        vizsgakerdesek = new ArrayList<>();
+        List<VizsgaValasz> vizsgavalaszok = new ArrayList<>();
         VizsgaValasz vizsgaValasz = null;
         for (String line : text) {
             Matcher m_kerdesszam = p_kerdesszam.matcher(line);
-            if (megoldasflag && m_kerdesszam.find() && m_kerdesszam.group(0).length()>0) {
-                megoldasflag=false;
-                kerdesSzam=Integer.parseInt(m_kerdesszam.group(1));
+            if (megoldasflag && m_kerdesszam.find() && m_kerdesszam.group(0).length() > 0) {
+                megoldasflag = false;
+                kerdesSzam = Integer.parseInt(m_kerdesszam.group(1));
+                kerdes = m_kerdesszam.group(2);
                 vizsgaValasz = new VizsgaValasz();
+                System.out.println(line);
                 continue;
             }
             System.out.println(line);
             Matcher m_valaszSzam = p_valaszSzam.matcher(line);
-            if(m_valaszSzam.find()){
+            if (m_valaszSzam.find()) {
                 valaszSzam = m_valaszSzam.group(1);
-                valasz=line.replaceFirst("^[a-m]\\)[ ]*", "");
-                valaszflag=true;
+                valasz = line.replaceFirst("^[a-m]\\)[ ]*", "");
+                valaszflag = true;
                 vizsgaValasz = new VizsgaValasz();
-                vizsgaValasz.setValaszSzam(valaszSzam);
-            } else {
-                valasz+=line;
                 vizsgaValasz.setValasz(valasz);
+                vizsgaValasz.setValaszSzam(valaszSzam);
+                vizsgavalaszok.add(vizsgaValasz);
+            } else {
+                valasz += line;
+                vizsgaValasz.setValaszSzam(valasz);
             }
             Matcher m2 = p_megoldas.matcher(line);
-            if (m2.find() && !megoldasflag && m2.group(0).length()>0) {
-                megoldasflag=true;
-                
-                System.out.println(String.join(",", m2.group(2).split(""))+"; groupcount:"+m2.groupCount());
+            if (m2.find() && !megoldasflag && m2.group(0).length() > 0) {
+                megoldasflag = true;
+                megoldas = m2.group(1);
+                VizsgaKerdes vizsgaKerdes = new VizsgaKerdes(kerdesSzam, kerdes);
+                for (VizsgaValasz v : vizsgavalaszok) {
+                    vizsgaKerdes.getValaszok().add(v);
+                }
+                vizsgaKerdes.setMegoldas(megoldas);
+                vizsgakerdesek.add(vizsgaKerdes);
+                vizsgavalaszok = new ArrayList<>();
+                System.out.println(String.join(",", m2.group(2).split("")) + "; groupcount:" + m2.groupCount());
             }
         }
     }
@@ -63,6 +77,6 @@ public class ImportKerdesek {
     public static void main(String[] args) {
         ImportKerdesek importkerdesek = new ImportKerdesek();
         importkerdesek.readFile();
-
+        System.out.println("");
     }
 }
--- alapozo/src/main/java/org/pallas/alapozo/VizsgaKerdes.java
+++ alapozo/src/main/java/org/pallas/alapozo/VizsgaKerdes.java
@@ -2,15 +2,21 @@ package org.pallas.alapozo;
 
 import java.util.ArrayList;
 import java.util.List;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
 public class VizsgaKerdes {
-    public int kerdesszam;
+
+    public int kerdesSzam;
     public String kerdes;
     public List<VizsgaValasz> valaszok = new ArrayList<>();
-    
-    public VizsgaKerdes(int kerdesszam, String kerdes) {
-        this.kerdesszam = kerdesszam;
+    public String megoldas;
+
+    public VizsgaKerdes(int kerdesSzam, String kerdes) {
+        this.kerdesSzam = kerdesSzam;
         this.kerdes = kerdes;
     }
-    
+
 }
