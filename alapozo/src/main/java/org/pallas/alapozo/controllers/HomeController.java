acdac1f6cdca64c8b735e7ac637bf8926910ad19
tenkesnet <rbardo@gmail.com>
2023-03-25 14:55:55 +0100

    Random. Kérdések megjelenítése

--- alapozo/src/main/java/org/pallas/alapozo/controllers/HomeController.java
+++ alapozo/src/main/java/org/pallas/alapozo/controllers/HomeController.java
@@ -2,6 +2,7 @@ package org.pallas.alapozo.controllers;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 import org.pallas.alapozo.model.Question;
 import org.pallas.alapozo.repository.IQuestionRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,11 +20,11 @@ public class HomeController {
     @GetMapping("/")
     public String vizsga(Model model) {
         List<Question> questions = new ArrayList<>();
-        questions.add(question_repo.findById(50).get());
-        questions.add(question_repo.findById(150).get());
-        questions.add(question_repo.findById(197).get());
-        questions.add(question_repo.findById(205).get());
-        
+        Random rnd = new Random();
+        for(int i=0; i<20; i++){
+            int q_random = rnd.nextInt(210)+1;
+            questions.add(question_repo.findById(q_random).get());
+        }
         model.addAttribute("kerdesek", questions);
         return "pages/vizsga";
     }
